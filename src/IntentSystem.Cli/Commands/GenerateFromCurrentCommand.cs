b2732bdec245ee7082a235f55817699766948197
Tomohisa Takaoka <ttakaoka@jtechs.com>
2026-04-07 23:30:09 -0700

    Add confirmed-rereview generate-from-current command

--- src/IntentSystem.Cli/Commands/GenerateFromCurrentCommand.cs
+++ src/IntentSystem.Cli/Commands/GenerateFromCurrentCommand.cs
@@ -172,6 +172,12 @@ internal static class GenerateFromCurrentCommand
         }
 
         if (args.Length > 0
+            && string.Equals(args[0], "confirmed-rereview", StringComparison.Ordinal))
+        {
+            return GenerateFromCurrentConfirmedRereviewCommand.Execute(context, args[1..], writer);
+        }
+
+        if (args.Length > 0
             && string.Equals(args[0], "clarify", StringComparison.Ordinal))
         {
             return GenerateFromCurrentClarifyCommand.Execute(context, args[1..], writer);
