4f06ad5697235d3d2a4b670b3e8c08447bfca1d1
Tomohisa Takaoka <ttakaoka@jtechs.com>
2026-04-07 23:52:16 -0700

    Add confirmed-closeout generate-from-current command

--- src/IntentSystem.Cli/Commands/GenerateFromCurrentCommand.cs
+++ src/IntentSystem.Cli/Commands/GenerateFromCurrentCommand.cs
@@ -184,6 +184,12 @@ internal static class GenerateFromCurrentCommand
         }
 
         if (args.Length > 0
+            && string.Equals(args[0], "confirmed-closeout", StringComparison.Ordinal))
+        {
+            return GenerateFromCurrentConfirmedCloseoutCommand.Execute(context, args[1..], writer);
+        }
+
+        if (args.Length > 0
             && string.Equals(args[0], "clarify", StringComparison.Ordinal))
         {
             return GenerateFromCurrentClarifyCommand.Execute(context, args[1..], writer);
